81012fcfadc16d684aa281644c1d86650802f95e
Tripti <Tripti@Triptis-MacBook-Air.local>
2016-08-22 15:18:17 +1000

    Added seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+u1 = User.create(:name=>"Triptii",:email_id=>"tripti.895@gmail.com", :password => "chicken" ,:password_confirmation=>"chicken")
+u2 = User.create(:name=>"Kym",:email_id=>"kym@ga.com", :password => "chicken" ,:password_confirmation=>"chicken")
+u3 = User.create(:name=>"Lori",:email_id=>"lori@ga.com", :password => "chicken" ,:password_confirmation=>"chicken")
+
+g1 = Group.create(:grp_name => 'Cousins')
+g1.grp_admin_id = u1.id
+
+g2 = Group.create(:grp_name => 'Hiking')
+g2.grp_admin_id = u2.id
+
+g3 = Group.create(:grp_name => 'BBQ')
+g3.grp_admin_id = u1.id
+
+m1 = Message.create(:content => 'Hi Cousins ')
+m1.user_id = u1
+m1.group_id = g1
+
+m2 = Message.create(:content => 'Welcome to this hiking group ')
+m2.user_id = u1
+m2.group_id = g2
+
+c1 = Contact.create(:user_id => u2.id , :group_id => g1.id)
+c2 = Contact.create(:user_id => u1.id , :group_id => g2.id)
+c3 = Contact.create(:user_id => u3.id , :group_id => g2.id)
